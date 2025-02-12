5d2cfb77562b8c5297f0a16f12eb3ba1f2f26c2d
Jari Antero Hiitola <jari.hiitola@student.hamk.fi>
2025-02-12 13:53:01 +0200

    Hiukan muutoksia admin-asioihin

---
+++ admin/index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <title>Pizzagate ylläpito</title>
+</head>
+<body>
+    <h1>Pizzagate-sivuston admin-alue</h1>
+    <br>
+    <a href="../index.html">Linkki indexiin / Paluu etusivulle</a>
+    <br>
+    <a href="tietokanta.php">Hallinnoi täytteitä</a>
+    <br>
+    <a href="randomizer.php">Satunnaisnappulan asetukset</a>
+    <br>
+    <a href="palautteet.php">Asiakaspalautteet</a>
+    <br>
+    <a href="tilastot.php">Tilastot täytteistä?</a>
+    <br>    
+    
+
+
+</body>
+</html>
